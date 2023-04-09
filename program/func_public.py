2bf29d97b2d6b8cae51add94ed9ed857b65fd94f
DomStarnini <jaman.x@gmail.com>
2023-04-09 20:47:24 +0200

    Stage 2Finito

--- program/func_public.py
+++ program/func_public.py
@@ -9,8 +9,81 @@ import time
 #get relevant period for iso from and to
 ISO_TIMES=get_ISO_times() 
 
-pprint(ISO_TIMES)
+#get historical candles
+def get_candles_historical(client, market):
+    
+    #define output
+    close_price = []
+    
+    #extract historical price from ISO_TIMES
+    for timeframe in ISO_TIMES.keys():
+        tf_obj = ISO_TIMES[timeframe]
+        from_iso = tf_obj["from_iso"]
+        to_iso = tf_obj["to_iso"]
+        
+        #protect API
+        time.sleep(0.2)
+    
+    #get data
+    candles = client.public.get_candles(
+        
+        market=market,
+        resolution=RESOLUTION,
+        from_iso = from_iso,
+        to_iso = to_iso,
+        limit = 100
+    ) 
+    
+    #structure data
+    for candle in candles.data["candles"]:
+        close_price.append({"datetime": candle["startedAt"], market : candle["close"]})   
+        
+    #construct and reverse dataframe
+    close_price.reverse()
+    return close_price   
+    
 
 #construct market prices
 def construct_market_prices(client):
-    pass
+    
+    #declare variables
+    tradeable_markets = []
+    markets = client.public.get_markets()
+    
+    #find tradeable pairs
+    for market in markets.data["markets"].keys():
+        market_info = markets.data["markets"][market]
+        if market_info["status"] == "ONLINE" and market_info["type"] == "PERPETUAL" :
+            tradeable_markets.append(market)
+    
+    #set initial dataframe
+    close_prices = get_candles_historical (client , tradeable_markets[0])
+    df = pd.DataFrame(close_prices)
+    df.set_index("datetime", inplace=True)
+    
+    for market in tradeable_markets [1:]:#in produzione
+        close_prices_add = get_candles_historical (client , market)
+        df_add = pd.DataFrame(close_prices_add)
+        df_add.set_index("datetime", inplace=True)
+        df = pd.merge(df, df_add, how="outer", on="datetime", copy=False)
+        del df_add
+        
+        nans = df.columns [df.isna().any()].tolist()
+        if len(nans) > 0 :
+            print ("Dropping columns : ")
+            print (nans)
+            df.drop(columns=nans, inplace=True)
+    
+   
+    return df
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- program/main.py
+++ program/main.py
@@ -3,6 +3,7 @@ from constants import ABORT_ALL_POSITION,FIND_COINTEGRATED
 from func_connections import connect_dydx
 from func_private import abort_all_positions
 from func_public import construct_market_prices
+from func_cointegration import store_cointegration_result
 
 if __name__=="__main__":
     #connect to client
@@ -18,7 +19,7 @@ if __name__=="__main__":
 #abort all position
 if ABORT_ALL_POSITION :
     try :
-        print ("Sto chiudendo tutte lo posizioni aperte")
+        print ("Sto chiudendo tutte lo posizioni aperte...")
         close_orders=abort_all_positions(client)
     except Exception as e:
         print("Errore di chiusura posizioni : ",e)    
@@ -26,9 +27,21 @@ if ABORT_ALL_POSITION :
         
 #find cointegrated pairs
 if FIND_COINTEGRATED : 
+    #construct market pairs
     try :
-        print ("Sto costruendo i prezzi di mercato aspetta 3 minuti")
+        print ("Sto costruendo i prezzi di mercato...")
         df_market_prices = construct_market_prices(client)
     except Exception as e:
         print("Errore di costruzione prezzi di mercato : ",e)    
         exit(1)              
+        
+    #store cointegrated pairs    
+    try :
+        print ("Sto salvando le coppie di coin...")
+        store_result = store_cointegration_result(df_market_prices)
+        if store_result != "saved" :
+            print ("Errore nel salvere le coppie di valute")
+            exit(1)
+    except Exception as e:
+        print("Errore nel salvere le coppie di valute : ",e)    
+        exit(1)      
